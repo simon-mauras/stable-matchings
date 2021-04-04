6291929c3a18fa753e5ccb0da424f3402f3cc7c1
Simon Mauras <simon.mauras@gmail.com>
2021-04-04 22:02:23 +0200

    stable pairs: multiplicity

--- StablePairs/exec.py
+++ StablePairs/exec.py
@@ -5,6 +5,7 @@ from subprocess import Popen, PIPE
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib.ticker import FixedLocator, FuncFormatter
 
 def transform(t):
   if type(t) == dict:
@@ -18,11 +19,15 @@ def transform(t):
     return np.array([transform(x) for x in t])
   return t
 
-def run(pop, S = 1000):
+def run(pop, multM = None, multW = None, S = 1000):
   nbMen, nbWomen = pop.shape
+  if multM == None: multM = [2] * nbMen
+  if multW == None: multW = [2] * nbWomen
   command = ["./main", str(S), str(nbMen), str(nbWomen)]
   p = Popen(command, stdin=PIPE, stdout=PIPE)
-  instance = " ".join(map(str, pop.reshape(-1)))
+  instance = ""
+  for line in [multM, multW] + list(pop):
+    instance += " ".join(map(str, line)) + "\n"
   output,_ = p.communicate(instance.encode())
   return transform(json.loads(output.decode()))
 
@@ -31,19 +36,27 @@ def min_nonzero(t):
 
 def imshow(t, norm=None, cmap=plt.cm.viridis):
   cmap.set_bad("black")
-  plt.imshow(t, origin="lower", norm=norm, cmap=cmap)
+  extent = (0, t.shape[1], 0, t.shape[0])
+  return plt.imshow(t, origin="lower", norm=norm, cmap=cmap, extent=extent)
 
-def errorbars(t, label=None):
+def errorbars(t, label=None, major=None):
   y, e = [], []
   for d in t:
-    mean = sum(v * i for i,v in d.items()) / sum(d.values())
-    var = sum(v * (i-mean)**2 for i,v in d.items()) / sum(d.values()) ** 2
-    y.append(mean)
-    e.append(5 * math.sqrt(var))
+    if any(d.values()):
+      mean = sum(v * i for i,v in d.items()) / sum(d.values())
+      var = sum(v * (i-mean)**2 for i,v in d.items()) / sum(d.values()) ** 2
+      y.append(mean)
+      e.append(5 * math.sqrt(var))
+    else:
+      y.append(0)
+      e.append(0)
   y, e = np.array(y), np.array(e)
-  plt.plot(range(len(t)), y, label=label)
-  plt.fill_between(range(len(t)), y-e, y+e,
-    alpha=0.1)
+  p = plt.errorbar(range(len(t)), y, e, fmt="o", markersize=1, label=label)
+  color = p[0].get_color()
+  for b in p[2]: b.set_alpha(0.3)
+  for a,b in zip(major, major[1:]+[len(t)]):
+    _x, _y, _e = range(a, b), y[range(a, b)], e[range(a, b)]
+    plt.plot(_x, _y, "-", color=color, linewidth=.5)
 
 def matrix_scaling(pop):
   nbMen, nbWomen = pop.shape
@@ -55,7 +68,7 @@ def matrix_scaling(pop):
     W, pop = W * row, pop / row.reshape((1, nbWomen))
   v = math.sqrt(W.mean() / M.mean())
   M, W = M * v, W / v
-  return M, W, pop
+  return [M, W, pop]
     
 
 def plot(data, target="plot.pdf"):
@@ -72,70 +85,120 @@ def plot(data, target="plot.pdf"):
   for d in data["ALL_nbW"]:
     v = d[0] if 0 in d else 0
     data["probaW"].append({0: v, 1: sum(d.values())-v})
+    
+  def set_ticks(ax, pos):
+    label = { pos[i]:str(i) for i in range(len(pos)) }
+    ax.set_minor_locator(FixedLocator(pos))
+    ax.set_major_locator(FixedLocator(pos, nbins=11))
+    ax.set_major_formatter(FuncFormatter(lambda x, pos: label[x]))
+    
+  ticksM = [0] + list(np.cumsum(data["multM"]))
+  ticksW = [0] + list(np.cumsum(data["multW"]))
   
   with PdfPages(target) as pdf:
     plt.cm.viridis.set_bad("black")
   
     ## plot popularities
+    idx = sum(([i] * m for i,m in enumerate(data["multM"])), [])
+    idy = sum(([i] * m for i,m in enumerate(data["multW"])), [])
+    popularity = data["popularity"][idx,:][:,idy]
     scaling = matrix_scaling(data["popularity"])
     last = scaling[2].max() - scaling[2].min() > 1e-3
+    scaling[2] = scaling[2][idx,:][:,idy]
     
     plt.figure(figsize=(14+4*last,4))
-    plt.subplot(1,3+last,1)
-    norm = LogNorm(min_nonzero(data["popularity"])/2, data["popularity"].max()*2)
-    plt.imshow(data["popularity"],
-      origin="lower", norm=norm, cmap=plt.cm.viridis)
+    ax = plt.subplot(1,3+last,1)
+    norm = LogNorm(min_nonzero(popularity)/2, popularity.max()*2)
+    imshow(popularity, norm=norm)
     plt.colorbar()
     plt.xlabel("woman's id")
     plt.ylabel("man's id")
+    set_ticks(ax.xaxis, ticksW)
+    set_ticks(ax.yaxis, ticksM)
     plt.title("popularity of each pair")
     
-    plt.subplot(1,3+last,2)
+    ax = plt.subplot(1,3+last,2)
     plt.xlabel("man's id")
     plt.ylabel("popularity")
-    #plt.yscale("log")
-    plt.plot(scaling[0])
+    set_ticks(ax.xaxis, ticksM)
+    plt.grid(which="major", linewidth=.5, alpha=.3)
+    X = [0] + list(np.cumsum(data["multM"]))
+    plt.plot(X[:-1], scaling[0], ".")
+    for i in range(data["nbMen"]):
+      if X[i+1] != X[i]:
+        plt.plot([X[i], X[i+1]-1], [scaling[0][i]]*2, color=plt.cm.tab10(0))
+    plt.yscale("log")
     
-    plt.subplot(1,3+last,3)
+    ax = plt.subplot(1,3+last,3)
     plt.xlabel("woman's id")
     plt.ylabel("popularity")
-    #plt.yscale("log")
-    plt.plot(scaling[1])
+    set_ticks(ax.xaxis, ticksW)
+    plt.grid(which="major", linewidth=.5, alpha=.3)
+    X = [0] + list(np.cumsum(data["multW"]))
+    plt.plot(X[:-1], scaling[1], ".")
+    for i in range(data["nbWomen"]):
+      if X[i+1] != X[i]:
+        plt.plot([X[i], X[i+1]-1], [scaling[1][i]]*2, color=plt.cm.tab10(0))
+    plt.yscale("log")
     
     if last:
-      plt.subplot(1,4,4)
-      plt.imshow(scaling[2], origin="lower", cmap=plt.cm.viridis)
+      ax = plt.subplot(1,4,4)
+      imshow(scaling[2])
       plt.colorbar()
       plt.xlabel("woman's id")
       plt.ylabel("man's id")
+      set_ticks(ax.xaxis, ticksW)
+      set_ticks(ax.yaxis, ticksM)
       plt.title("normalized popularity of each pair")
     
     plt.tight_layout()
     pdf.savefig()
     
+    """
+    ## plot multiplicities
+    if set(data["multW"]) != {1} or set(data["multW"]) != {1}:
+      fig = plt.figure(figsize=(10,4))
+      
+      ax = plt.subplot(1,2,1)
+      plt.xlabel("man's id")
+      plt.ylabel("multiplicity")
+      plt.plot(data["multM"], ".-")
+      
+      ax = plt.subplot(1,2,2)
+      plt.xlabel("woman's id")
+      plt.ylabel("multiplicity")
+      plt.plot(data["multW"], ".-")
+      
+      plt.tight_layout()
+      pdf.savefig()
+    """
+    
     ## plot extremal matchings
     fig = plt.figure(figsize=(14,4))
-    norm = LogNorm(1/S,1)
-    plt.subplot(1,3,1)
+    norm = LogNorm(1/(data["nbMen"]*data["nbWomen"]),1)
+    ax = plt.subplot(1,3,1)
     plt.title("Pr[matched in some matching]")
-    im = plt.imshow(data["ALL_couples"],
-      origin="lower", norm=norm, cmap=plt.cm.viridis)
+    im = imshow(data["ALL_couples"], norm=norm)
+    set_ticks(ax.xaxis, ticksW)
+    set_ticks(ax.yaxis, ticksM)
     plt.xlabel("woman's id")
     plt.ylabel("man's id")
     plt.colorbar()
     
-    plt.subplot(1,3,2)
-    plt.title("Pr[matched in WOSM]")
-    plt.imshow(data["WOSM_couples"],
-      origin="lower", norm=norm, cmap=plt.cm.viridis)
-    plt.xlabel("woman's id")
-    plt.ylabel("man's id")
-    plt.colorbar()
-    
-    plt.subplot(1,3,3)
+    ax = plt.subplot(1,3,2)
     plt.title("Pr[matched in MOSM]")
-    plt.imshow(data["MOSM_couples"],
-      origin="lower", norm=norm, cmap=plt.cm.viridis)
+    imshow(data["MOSM_couples"], norm=norm)
+    set_ticks(ax.xaxis, ticksW)
+    set_ticks(ax.yaxis, ticksM)
+    plt.xlabel("woman's id")
+    plt.ylabel("man's id")
+    plt.colorbar()
+    
+    ax = plt.subplot(1,3,3)
+    plt.title("Pr[matched in WOSM]")
+    imshow(data["WOSM_couples"], norm=norm)
+    set_ticks(ax.xaxis, ticksW)
+    set_ticks(ax.yaxis, ticksM)
     plt.xlabel("woman's id")
     plt.ylabel("man's id")
     plt.colorbar()
@@ -144,82 +207,84 @@ def plot(data, target="plot.pdf"):
     pdf.savefig()
     
     ## plot extremal matchings
-    plt.figure(figsize=(10,4))
-    # women's rank
-    plt.subplot(1,2,1)
-    plt.title("outcomes for women")
-    plt.ylim((0, data["nbMen"]))
-    plt.xlabel("woman's id")
-    plt.ylabel("expected rank of partner")
-    errorbars(data["WOSM_rankW"], "WOSM")
-    errorbars(data["MOSM_rankW"], "MOSM")
-    plt.legend()
-    """
-    plt.twinx()
-    plt.ylim((0, 1))
-    plt.ylabel("probability of being matched")
-    errorbars(data["probaW"])
-    """
+    fig = plt.figure(figsize=(10,4))
     # men's rank
-    plt.subplot(1,2,2)
+    ax = plt.subplot(1,2,1)
     plt.title("outcomes for men")
     plt.ylim((0, data["nbWomen"]))
     plt.xlabel("man's id")
-    plt.ylabel("expected rank of partner")
-    errorbars(data["WOSM_rankM"], "WOSM")
-    errorbars(data["MOSM_rankM"], "MOSM")
+    plt.ylabel("E[rank partner]")
+    set_ticks(ax.xaxis, ticksM)
+    errorbars(data["WOSM_rankM"], "WOSM", ticksM[:-1])
+    errorbars(data["MOSM_rankM"], "MOSM", ticksM[:-1])
+    plt.grid(which="major", linewidth=.5, alpha=.3)
     plt.legend()
-    """
-    plt.twinx()
-    plt.ylim((0, 1))
-    plt.ylabel("probability of being matched")
-    errorbars(data["probaM"])
-    """
+    # women's rank
+    ax = plt.subplot(1,2,2)
+    plt.title("outcomes for women")
+    plt.ylim((0, data["nbMen"]))
+    plt.xlabel("woman's id")
+    plt.ylabel("E[rank partner]")
+    set_ticks(ax.xaxis, ticksW)
+    errorbars(data["WOSM_rankW"], "WOSM", ticksW[:-1])
+    errorbars(data["MOSM_rankW"], "MOSM", ticksW[:-1])
+    plt.grid(which="major", linewidth=.5, alpha=.3)
+    plt.legend()
     # saving fig
     plt.tight_layout()
     pdf.savefig()
     
-    
     ## plot number partners
-    maxi = 1
-    maxi = max(maxi, max(max(d.keys()) for d in data["ALL_deltaW"]))
-    maxi = max(maxi, max(max(d.keys()) for d in data["ALL_deltaM"]))
     plt.figure(figsize=(10,4))
-    # women's rank
-    plt.subplot(1,2,1)
-    plt.title("outcomes for women")
-    plt.xlabel("woman's id")
-    plt.axhline(1 + math.log(data["nbMen"]),
-      label="1 + log(nbMen)", color="k", linestyle="--")
-    errorbars(data["ALL_deltaW"], "1 + delta rank worst/best partner")
-    errorbars(data["ALL_nbW"], "number of stable partners")
-    plt.ylim(bottom=0)
-    plt.legend()
     # men's rank
-    plt.subplot(1,2,2)
+    ax = plt.subplot(1,2,1)
     plt.title("outcomes for men")
     plt.xlabel("man's id")
+    set_ticks(ax.xaxis, ticksM)
     plt.axhline(1 + math.log(data["nbWomen"]),
       label="1 + log(nbWomen)", color="k", linestyle="--")
-    errorbars(data["ALL_deltaM"], "1 + delta rank worst/best partner")
-    errorbars(data["ALL_nbM"], "number of stable partners")
+    errorbars(data["ALL_deltaM"], "1 + delta rank worst/best partner", ticksM[:-1])
+    errorbars(data["ALL_nbM"], "number of stable partners", ticksM[:-1])
+    plt.grid(which="major", linewidth=.5, alpha=.3)
+    plt.ylim(bottom=0)
+    plt.legend()
+    # women's rank
+    ax = plt.subplot(1,2,2)
+    plt.title("outcomes for women")
+    plt.xlabel("woman's id")
+    set_ticks(ax.xaxis, ticksW)
+    plt.axhline(1 + math.log(data["nbMen"]),
+      label="1 + log(nbMen)", color="k", linestyle="--")
+    errorbars(data["ALL_deltaW"], "1 + delta rank worst/best partner", ticksW[:-1])
+    errorbars(data["ALL_nbW"], "number of stable partners", ticksW[:-1])
+    plt.grid(which="major", linewidth=.5, alpha=.3)
     plt.ylim(bottom=0)
     plt.legend()
     # saving fig
     plt.tight_layout()
     pdf.savefig()
 
-M, W = 100, 100
-#p, q = [1] * (M//2) + [2] * (M//2), [1] * (W//2) + [2] * (W//2)
-p = [ 0.99 ** i for i in range(M) ]
+
+# each person has capacity 1 or 2.
+M, W = 50, 50
+capa = [random.randint(1,2) for i in range(M)]
+mult = (capa, list(capa))
+random.shuffle(mult[1])
+p = [ 0.9 ** i for i in range(M) ]
 q = [ 0.95 ** i for i in range(W) ]
-#p = [ (i+10) ** .1 for i in reversed(range(M)) ]
-#q = [ (i+10) ** -.1 for i in range(W) ]
+
+# men are students (capacity = 1), women are schools (capacity > 1)
+capa = [ 3+i for i in range(10) ]
+M, W = int(1.2*sum(capa)), len(capa)
+mult = ([1]*M, capa)
+p = [ 0.95 ** i for i in range(M) ]
+q = [ 0.5 ** i for i in range(W) ]
+
 popularity = np.zeros((M, W))
 for i in range(M):
   for j in range(W):
-    if random.random() > 0.:
+    if random.random() > 0.05:
       popularity[i][j] = p[i] * q[j]  
       #popularity[i][j] = (i-M//2) ** 2 + (j-W//2) ** 2
       #popularity[i][j] = .1 + math.sin(i*5*math.pi/M) ** 2 + (j/W - 1/2) ** 2
-plot(run(popularity, S=1000))
+plot(run(popularity, *mult, S=10000))
--- StablePairs/main.cpp
+++ StablePairs/main.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <set>
 #include <functional>
 #include <algorithm>
 #include <cassert>
@@ -15,6 +16,7 @@ using vi = vector<int>;
 using vd = vector<double>;
 using vvi = vector<vi>;
 using vvd = vector<vd>;
+#define UNIQUE(t) t.resize(distance(t.begin(), unique(t.begin(), t.end())))
 
 template<typename T>
 ostream& operator<<(ostream& out, const vector<T> &t) {
@@ -61,27 +63,48 @@ int main(int argc, char** argv)
   
   cerr << "running simulations..." << endl;
   
+  // read input
+  vi multM(nbMen, 0), multW(nbWomen, 0);
+  for (int i=0; i<nbMen; i++)
+    cin >> multM[i];
+  for (int j=0; j<nbWomen; j++)
+    cin >> multW[j];
   vvd popularity(nbMen, vd(nbWomen, 0));
   for (int i=0; i<nbMen; i++)
     for (int j=0; j<nbWomen; j++)
       cin >> popularity[i][j];
   
+  // multiplicities
+  vi cumulM = multM, cumulW = multW;
+  for (int i=1; i<nbMen; i++)
+    cumulM[i] += cumulM[i-1];
+  for (int j=1; j<nbWomen; j++)
+    cumulW[j] += cumulW[j-1];
+  auto getIdM = [&cumulM](int m) {
+    auto it = upper_bound(cumulM.begin(), cumulM.end(), m);
+    return distance(cumulM.begin(), it);
+  };
+  auto getIdW = [&cumulW](int w) {
+    auto it = upper_bound(cumulW.begin(), cumulW.end(), w);
+    return distance(cumulW.begin(), it);
+  };
+  
   // all stable pairs
-  vvi data_ALL_couples(nbMen, vi(nbWomen, 0));
-  vector<map<int,int>> data_ALL_nbM(nbMen);
-  vector<map<int,int>> data_ALL_nbW(nbWomen);
-  vector<map<int,int>> data_ALL_deltaM(nbMen);
-  vector<map<int,int>> data_ALL_deltaW(nbWomen);
+  vvi data_ALL_couples(cumulM.back(), vi(cumulW.back(), 0));
+  vector<map<int,int>> data_ALL_nbM(cumulM.back());
+  vector<map<int,int>> data_ALL_nbW(cumulW.back());
+  vector<map<int,int>> data_ALL_deltaM(cumulM.back());
+  vector<map<int,int>> data_ALL_deltaW(cumulW.back());
   
   // man optimal stable matching
-  vvi data_MOSM_couples(nbMen, vi(nbWomen, 0));
-  vector<map<int,int>> data_MOSM_rankM(nbMen);
-  vector<map<int,int>> data_MOSM_rankW(nbWomen);
+  vvi data_MOSM_couples(cumulM.back(), vi(cumulW.back(), 0));
+  vector<map<int,int>> data_MOSM_rankM(cumulM.back());
+  vector<map<int,int>> data_MOSM_rankW(cumulW.back());
   
   // woman optimal stable matching
-  vvi data_WOSM_couples(nbMen, vi(nbWomen, 0));
-  vector<map<int,int>> data_WOSM_rankM(nbMen);
-  vector<map<int,int>> data_WOSM_rankW(nbWomen);
+  vvi data_WOSM_couples(cumulM.back(), vi(cumulW.back(), 0));
+  vector<map<int,int>> data_WOSM_rankM(cumulM.back());
+  vector<map<int,int>> data_WOSM_rankW(cumulW.back());
   
   for (int s=0; s<S; s++)
   {
@@ -92,59 +115,94 @@ int main(int argc, char** argv)
       cerr << "Generation of preferences..." << endl;
     
     vvi prefMen, prefWomen;
-    tie(prefMen, prefWomen) = generateSymetric(generator, popularity);
+    tie(prefMen, prefWomen) =
+      generateSymetric(generator, popularity);
+    
+    // multiplicities
+    vvi prefMenMult, prefWomenMult;
+    for (int i=0; i<nbMen; i++)
+    {
+      vi pref;
+      for (auto w: prefMen[i])
+        for (int j=cumulW[w]-multW[w]; j<cumulW[w]; j++)
+          pref.push_back(j);
+      for (int r=0; r<multM[i]; r++)
+        prefMenMult.push_back(pref);
+    }
+    for (int j=0; j<nbWomen; j++)
+    {
+      vi pref;
+      for (auto m: prefWomen[j])
+        for (int i=cumulM[m]-multM[m]; i<cumulM[m]; i++)
+          pref.push_back(i);
+      for (int r=0; r<multW[j]; r++)
+        prefWomenMult.push_back(pref);
+    }
     
     if (S == 1)
       cerr << "Computing stable husbands..." << endl;
     
-    GaleShapley gs(prefMen, prefWomen);
+    GaleShapley gs(prefMenMult, prefWomenMult);
     gs.computeStableHusbands();
     
     if (S == 1)
       cerr << "Output results" << endl;
-      
-    vvi wifes(nbMen);
-    for (int j=0; j<nbWomen; j++)
+    
+    vvi wifes(cumulM.back());
+    for (int j=0; j<cumulW.back(); j++)
     {
-      data_ALL_nbW[j][gs.husbands[j].size()]++;
+      set<int> types;
+      for (int i: gs.husbands[j])
+        types.insert(getIdM(i));
+      data_ALL_nbW[j][types.size()]++;
       if (!gs.husbands[j].empty())
       {
         vi order(nbMen, -1);
-        for (int i=0; i<(int)prefWomen[j].size(); i++)
-          order[prefWomen[j][i]] = i;
+        int w = getIdW(j);
+        for (int r=0; r<(int)prefWomen[w].size(); r++)
+          order[prefWomen[w][r]] = r;
         
         int mosm = gs.husbands[j].front();
         int wosm = gs.husbands[j].back();
+        int mosm_rank = order[getIdM(mosm)];
+        int wosm_rank = order[getIdM(wosm)];
         data_MOSM_couples[mosm][j]++;
         data_WOSM_couples[wosm][j]++;
-        data_MOSM_rankW[j][order[mosm]]++;
-        data_WOSM_rankW[j][order[wosm]]++;
-        data_ALL_deltaW[j][1 + order[mosm] - order[wosm]]++;
-        for (auto i : gs.husbands[j])
+        data_MOSM_rankW[j][mosm_rank]++;
+        data_WOSM_rankW[j][wosm_rank]++;
+        data_ALL_deltaW[j][1 + mosm_rank - wosm_rank]++;
+        for (int i:gs.husbands[j])
         {
           data_ALL_couples[i][j]++;
           wifes[i].push_back(j);
         }
       }
     }
-    for (int i=0; i<nbMen; i++)
+    for (int i=0; i<cumulM.back(); i++)
     {
-      data_ALL_nbM[i][wifes[i].size()]++;
+      set<int> types;
+      for (int j: wifes[i])
+        types.insert(getIdW(j));
+      data_ALL_nbM[i][types.size()]++;
       if (!wifes[i].empty())
       {
         vi order(nbWomen, -1);
-        for (int j=0; j<(int)prefMen[i].size(); j++)
-          order[prefMen[i][j]] = j;
+        int m = getIdM(i);
+        for (int r=0; r<(int)prefMen[m].size(); r++)
+          order[prefMen[m][r]] = r;
         
-        sort(wifes[i].begin(), wifes[i].end(), [order](int a, int b) {
-          return order[a] < order[b];
+        sort(wifes[i].begin(), wifes[i].end(), [&order,&getIdW](int a, int b) {
+          int i = getIdW(a), j = getIdW(b);
+          return i != j ? order[i] < order[j] : a < b;
         });
         
         int mosm = wifes[i].front();
         int wosm = wifes[i].back();
-        data_MOSM_rankM[i][order[mosm]]++;
-        data_WOSM_rankM[i][order[wosm]]++;
-        data_ALL_deltaM[i][1 + order[wosm] - order[mosm]]++;
+        int mosm_rank = order[getIdW(mosm)];
+        int wosm_rank = order[getIdW(wosm)];
+        data_MOSM_rankM[i][mosm_rank]++;
+        data_WOSM_rankM[i][wosm_rank]++;
+        data_ALL_deltaM[i][1 + wosm_rank - mosm_rank]++;
       }
     }
   }
@@ -154,6 +212,8 @@ int main(int argc, char** argv)
   
   cout << "{\"nbMen\":" << nbMen << ",\"nbWomen\":" << nbWomen << ",\n";
   cout << "\"popularity\":" << popularity << ",\n";
+  cout << "\"multM\":" << multM << ",\n";
+  cout << "\"multW\":" << multW << ",\n";
   cout << "\"ALL_couples\":" << data_ALL_couples << ",\n";
   cout << "\"ALL_nbM\":" << data_ALL_nbM << ",\n";
   cout << "\"ALL_nbW\":" << data_ALL_nbW << ",\n";
